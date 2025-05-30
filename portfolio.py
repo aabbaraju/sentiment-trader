f8733bfa1e44755c3483c4810714ea0067943b67
Aryan <aryanabbaraju@gmail.com>
2025-05-30 17:57:56 -0400

    ytfinance implementation

--- portfolio.py
+++ portfolio.py
@@ -1,9 +1,18 @@
 import datetime
+import yfinance as yf
 
 portfolio = {}
 trade_history  = []
 
+def get_price(ticker):
+    try:
+        stock = yf.Ticker(ticker)
+        return stock.info['regularMarketPrice']
+    except Exception as e:
+        print(f"Error fetching market price for {ticker}: {e}")
+        return 0
 def execute_trade(ticker, action, qty=1, price = 100.0):
+    price = get_price(ticker)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H: %M: %S")
 
     if action == "BUY":
@@ -16,7 +25,7 @@ def execute_trade(ticker, action, qty=1, price = 100.0):
         "Ticker": ticker, 
         "Action": action, 
         "Quantity": qty, 
-        "Price": price
+        "Price": round(price, 2)
     })
 
-    return f"{action} executed for {ticker}", portfolio.copy(), trade_history.copy()
+    return f"{action} executed for {ticker} at ${round(price,2)}" , portfolio.copy(), trade_history.copy()
